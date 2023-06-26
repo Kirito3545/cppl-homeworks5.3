3cace30c2182ea1ae5feb5300f9c37e2fa838b9a
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-06-26 03:03:21 +0300

    Initial commit

---
+++ OddNumbers.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+class OddNumbers
+{
+private:
+	int count_ = 0;
+	int sum_ = 0;
+
+public:
+	OddNumbers() = default;
+
+	void operator ()(int value);
+	int getSum();
+	int getCount();
+};
